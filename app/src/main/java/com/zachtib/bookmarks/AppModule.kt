e603a70c8e56b94ddd4e8ab42162adeed14fd855
Zach Tibbitts <zachtib@gmail.com>
2019-02-25 17:55:19 -0500

    was this really necessary?

--- app/src/main/java/com/zachtib/bookmarks/AppModule.kt
+++ app/src/main/java/com/zachtib/bookmarks/AppModule.kt
@@ -3,6 +3,7 @@ package com.zachtib.bookmarks
 import android.content.Context
 import androidx.room.Room
 import com.zachtib.bookmarks.db.BookmarksDatabase
+import com.zachtib.bookmarks.db.createDatabase
 import com.zachtib.bookmarks.service.BookmarksService
 import com.zachtib.bookmarks.ui.addaccount.AddAccountViewModel
 import com.zachtib.bookmarks.ui.bookmarklist.BookmarkListViewModel
@@ -15,7 +16,7 @@ val appModule = module {
     single { get<Context>().getSharedPreferences("bookmarks-prefs", Context.MODE_PRIVATE) }
     single { BookmarksPreferences(get()) }
     single { BookmarksService(get(), get()) }
-    single { Room.databaseBuilder(get(), BookmarksDatabase::class.java, "bookmarks-db").build() }
+    single { createDatabase<BookmarksDatabase>(get(), "bookmarks-db") }
 
     // Use cases
     single<OpenBookmarkUseCase> { OpenBookmarkImpl(get()) }
